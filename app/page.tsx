078e03a843d01266d3ef8eef421011e986007171
Heshan <heshankavinda0908@gmail.com>
2025-01-02 12:32:50 +0530

    hello

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
-    <div className="text-3x1">
-      Hello!<br />
-      Welcome My Blog
-=======
     <div className="text-5xl text-center "> <span className="text-7xl">Hello! </span><br />
       welcome my motivation blog
->>>>>>> 955b1b4 (new)
     </div>
   );
 }
